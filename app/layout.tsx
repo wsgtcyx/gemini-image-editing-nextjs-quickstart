0376b35336335693983f528c9962e4e5e9e710b3
wsgtcyx <439248005@qq.com>
2025-03-18 15:40:13 +0800

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+			<script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qq1g0rh84d");
+              `,
+          }}
+        />
+      </head>
       <body
         className={`${openSans.className} antialiased bg-white dark:bg-slate-950`}
         suppressHydrationWarning
